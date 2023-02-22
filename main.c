e7bcc011cf11404ce3d7a157ba142e1c63c50d01
Luke Hayes <lukepurplehayes@gmail.com>
2023-02-22 09:25:40 +0000

    Glad initialized. Now OPENGL functions can be called.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
@@ -9,6 +10,11 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+
+    // Set OpenGL
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(800, 600, "app", NULL, NULL);
@@ -21,12 +27,18 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    // Initialize GLAD
+     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+
+     float c = 0.0;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        c += 0.1;
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.5, 0.5, 0.5, sin(c));
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
